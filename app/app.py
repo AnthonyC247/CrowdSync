c23d3e89ea884b3fdfb342e15387b18ac40a5e82
kjx172 <kisituaaron@gmail.com>
2024-07-10 17:51:04 -0500

    added page change and result cards

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from datetime import datetime
 from pprint import pprint
 from ticketmasterapi import search_events
+from flask_session import Session
 
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'  # Configure session to use the filesystem
+Session(app)
 
 def validateInput(zip_city, start_date, end_date):
     #checks if zip_city is a zipcode
@@ -41,18 +44,29 @@ def home():
             return render_template('home.html', error=errorstring)
 
         event_info= search_events(zip_city, start_date, end_date, query)
-        pprint(event_info)
-        #return redirect(url_for('results', event_data=event_data))
+        session['event_info'] = event_info
+        return redirect(url_for('results'))
     
     return render_template('home.html')
 
-@app.route("/results")
+@app.route("/results", methods=["GET", "POST"])
 def results():
-    event_data = request.args.get('event_data')  # Retrieve event_data from query parameters
+    if request.method == "POST":
+        zip_city = request.form.get("zip_city")
+        start_date = request.form.get("start_date")
+        end_date = request.form.get("end_date")
+        query = request.form.get("query")
+        
+        errorstring = validateInput(zip_city, start_date, end_date)
+        if errorstring:
+            return render_template('results.html', error=errorstring)
 
-    print(event_data)# CHANGE TO PROCESS EVENT DATA
-
-    return render_template('results.html', event_data=event_data)
+        event_info= search_events(zip_city, start_date, end_date, query)
+        session['event_info'] = event_info
+        return redirect(url_for('results'))
+    
+    event_info = session.get('event_info')
+    return render_template('results.html', event_info=event_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
